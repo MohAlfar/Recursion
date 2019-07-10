082368505c9f8d223cfe54582276d1a55c7c767b
MohAlfar <mohdkhaled.alfar@gmail.com>
2019-07-10 17:28:51 +0300

    ex12

---
+++ Untitled-1.js
@@ -0,0 +1,369 @@
+/*
+1
+Write a function named tellFortune that:
+takes 4 arguments: number of children, 
+partner's name, geographic location, job title.
+outputs your fortune to the screen like so: 
+"You will be a X in Y, and married to Z with N kids."
+*/
+ function tellFortune(a,b,c,d){
+
+    
+ }
+
+
+   
+   /*
+   2
+   Write a function named calculateDogAge that:
+   takes 1 argument: your puppy's age.
+   calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
+   outputs the result to the screen like so: "Your doggie is NN years old in dog years!"
+   Ex: calculateDogAge(1);
+   => "Your doggie is 7 years old in dog years!"
+   */
+   
+   
+   /*
+   3
+   Write a function named calculateSupply that:
+   takes 2 arguments: age, amount per day.
+   calculates the amount consumed for rest of the life (based on a constant max age 100).
+   outputs the result to the screen like so:
+   "You will need NN to last you until the ripe old age of X"
+   Ex: calculateSupply(30, 3);
+   => 'You will need 76650 cups of tea to last you until the ripe old age of 100;
+   */
+   
+   /*
+   4
+   Write a function called greet that:
+   takes 1 argument: name.
+   and it will return hello + name
+   Ex: greet("Jouza")
+   => "Hello Jouza"
+   */
+   
+   
+   
+   
+   // 5
+   // what is the error:
+   
+   
+   
+   
+   
+   // 6
+   // fix these functions:
+   
+   
+   /*
+   7
+   Write a function called cube that:
+   accept 1 parameter and calculate the cube of this number
+   Ex: cube(4)
+   => 64
+   */
+    
+   
+   /*
+   Write a function called multiply that:
+   accept 2 parameters and calculate the multiply of these 2 numbers
+   Ex: multiply(3,4)
+   => 12
+   */
+   
+   
+   
+   /*
+   Write a function called canIGetADrivingLicense that:
+   accept 1 parameter represent the age
+   and if the age greater than or equal to 20 return "yes you can"
+   otherwise return "please come back after X years to get one"
+   Ex: canIGetADrivingLicense(21)
+   => "yes you can"
+   Ex: canIGetADrivingLicense(17)
+   => "please come back after 3 years to get one"
+   */
+   
+   
+   
+   /*
+   10
+   Write a function called sameLength
+   that accepts two strings as arguments,
+   and returns true if those strings have the same length, and false otherwise.
+   **hint: how we can know string length   Ex: : "tree".length   => 4
+   Ex: sameLength("tree","clue")
+   => true
+   Ex: sameLength("tree","car")
+   => false
+   
+   */
+   
+   
+   
+   /*
+   11
+   Write a function called largerNubmer
+   that accept two numbers as arguments,
+   and return the first larger numbers
+   Ex: largerNubmer(5,6)
+   => 6
+   Ex: largerNubmer(5,3)
+   => 5
+   */
+   
+   
+   
+   /*
+   12
+   Write a function called smallerNubmer
+   that accept three numbers as arguments,
+   and return the first smaller number
+   Ex: smallerNubmer(8,6,7)
+   => 6
+   Ex: smallerNubmer(5,99,34)
+   => 5
+   */
+  
+   
+   /*
+   13
+   Write a function called shorterString
+   that accept five string as an arguments,
+   and return the first shorter string
+   Ex: shorterString("air","school","car","by","github")
+   => by
+   */
+   
+  
+   
+   /*
+   14
+   Write a function called longerString
+   that accept four string as an arguments,
+   and return the first longer string
+   Ex: shorterString("air","school","car","github")
+   => school
+   */
+   
+   
+   
+   
+   
+   
+   
+   /*
+   15
+   Write a function called isEven
+   that accept 1 argument as a number,
+   and return true if this number is even
+   and false if this number is odd
+   Ex: isEven(1)
+   => false
+   Ex: isEven(2)
+   => true
+   */
+  
+   
+   
+   /*
+   16
+   Write a function called isOdd
+   that accept 1 argument as a number,
+   and return true if this number is Odd
+   and false if this number is Even
+   Ex: isOdd(4)
+   => false
+   Ex: isOdd(5)
+   => true
+   */
+   
+   /*
+   17
+   Write a function called positive
+   that accept 1 argument as a number,
+   and return the positive version of the number passed
+   Ex: positive(4)
+   => 4
+   Ex: positive(-5)
+   => 5
+   */
+  
+   
+   /*
+   18
+   Write a function called fullName
+   that accept two parameters, firstName and lastName,
+   and returns the firstName and lastName concatenated
+   together with a space in between.
+   Ex: fullName("Mohammad","Jouza")
+   => "Mohammad Jouza"
+   Ex: fullName("Alex", "Mercer")
+   => "Alex Mercer"
+   */
+   
+   
+   /*
+   19
+   Write a function called average
+   that takes five numbers as inputs
+   and returns the average of those numbers.
+   Ex: average(1,2,3,4,5)
+   => 3
+   Ex: average(5,7,9,3,5)
+   => 5.8
+   */
+   
+   /*
+   20
+   Write a function called randomNumber
+   that didnt takes any parameter
+   and returns a random number between 0-1
+   ** hint: you can seacrh using MDN
+   Ex: randomNumber()
+   => 0.2278
+   Ex: randomNumber()
+   => 0.475
+   */
+   
+   
+   /*
+   21
+   Write a function called randomBetweenNumbers
+   that takes 2 parameters
+   and returns a random number between them
+   ** hint: you can seacrh using MDN
+   Ex: randomBetweenNumbers(1,8)
+   => 7.5412
+   Ex: randomBetweenNumbers(3,100)
+   => 23
+   */
+   
+   
+   /*
+   22
+   Write a function called scoreInUniversty
+   that takes 1 parameters
+   and returns the alpabet in the unevirsty
+   A => 95-100
+   B => 85-94
+   C => 70-84
+   D=> 50-69
+   F=> 0-49
+   Ex: scoreInUniversty(96)
+   => "A"
+   Ex: randomBetweenNumbers(3)
+   => "F"
+   */
+
+   /*
+   23
+   Write a function called counter
+   that will returns a number bigger
+   than the one that returnd before
+   and start from 0
+   Ex: counter()
+   => 1
+   Ex: counter()
+   => 2
+   Ex: counter()
+   => 3
+   */
+   
+   
+   /*
+   24
+   Write a function called resetCounter
+   that will reset the previuos function
+   and return the number before reset and
+   a string say that the counter reset
+   Ex: counter()
+   => 1
+   Ex: counter()
+   => 2
+   Ex: counter()
+   => 3
+   Ex: resetCounter()
+   => 3 and the counter reset now
+   Ex: counter()
+   => 1
+   Ex: counter()
+   => 2
+   Ex: resetCounter()
+   => 2 and the counter reset now
+   Ex: counter()
+   => 1
+   */
+   // var x=0;
+   
+   
+   
+   
+   
+   
+   /*
+   25
+   // this question is in progress wait
+   Write a function called guessMe
+   that will take an argument (number)
+   if the number not between 0 and 5 [0,1,2,3,4]
+   it will return "Please insert a number between 0 and 5"
+   if it is between 0 and 5, and guessed the correct number 0-5 [0,1,2,3,4]
+   will return "you guess me correctly"
+   if it is between 0 and 5, and not the same number randomly 0-5 [0,1,2,3,4]
+   will return "you didnt guess me I was 3"
+   Ex: guessMe(0)
+   => "you didnt guess me I was 3"
+   Ex: guessMe(10)
+   =>"Please insert a number between 0 and 5"
+   Ex: guessMe(-5)
+   => "Please insert a number between 0 and 5"
+   Ex: guessMe(3)
+   => "you guess me correctly"
+   */
+   
+   
+   /*
+   26
+   // this question is in progress wait
+   Write a function called guessMe
+   that will take an argument (number)
+   if the number not between 0 and 5 [0,1,2,3,4]
+   it will return "Please insert a number between 0 and 5"
+   if it is between 0 and 5, and guessed the correct number 0-5 [0,1,2,3,4]
+   will return "you guess me correctly"
+   if it is between 0 and 5, and not the same number randomly 0-5 [0,1,2,3,4]
+   will return "you didnt guess me I was 3"
+   Ex: guessMe(0)
+   => "you didnt guess me I was 3"
+   Ex: guessMe(10)
+   =>"Please insert a number between 0 and 5"
+   Ex: guessMe(-5)
+   => "Please insert a number between 0 and 5"
+   Ex: guessMe(3)
+   => "you guess me correctly"
+   */
+   
+   
+   /*
+   */
+   
+   
+   /*
+   */
+   
+   
+   /*
+   */
+   
+   
+   /*
+   */
+   
+   
+   
+   
+   /////////// Advanced part
--- recursion.js
+++ recursion.js
@@ -9,8 +9,13 @@ Ex: sum(5); => 5 + 4 + 3 + 2 + 1 + 0 => 15
 
 */
 function sum(n) {
+  if (n==0){
+    return 0;
+  }
+  return n+sum(n-1);
   // your code here 
 }
+console.log(sum(4));
 
 /*
 2
@@ -23,9 +28,16 @@ Ex: factorial(4); => 4 * 3 * 2 * 1 => 24
 
 */
 function factorial(n) {
-  // your code here 
-}
 
+  if(n===1){
+
+    return 1;
+  
+  }
+return n =n* factorial(n-1);
+ 
+}
+console.log(factorial(4))
 
 /*
 3
@@ -36,7 +48,16 @@ and return the string number time with space
 Ex: repeatStr("to",2); => "to to"
 Ex: repeatStr("to",4); => "to to to to"
 */
+function repeatStr(x,y){
 
+if (y===0){
+  return"";
+}
+return x+ repeatStr(x,y-1)
+ 
+}
+
+console.log(repeatStr("mohd ",8))
 
 /*
 4
@@ -53,109 +74,262 @@ fib(n): 1 1 2 3 5 8 13 21
         | | | | | |  |  |
 n:      0 1 2 3 4 5  6  7
 */
+function ficccccbon(x){
+
+  if(x===1||x===0){
+    return 1;
+  }
+return ficccccbon(x-1)+ficccccbon(x-2);
+
+}
+console.log(ficccccbon(6));
+
 
 
 /*
-
-
-
-
+5
+Create a function called sum2
+that take two parameter
+and will return the sumation from the first number to the second number
+Ex: sum2(4, 5); => 4 + 5 => 9
+Ex: sum2(3, 6); => 3 + 4 + 5 + 6 => 18
 */
+
+function sum2(num1,num2){
+
+  if(num1===num2){
+  
+    return num2;
+  }
+  
+  return (num1 + sum2(  (num1+1) , num2  )  );
+  }
+  console.log(sum2(5,5));
+  
+  
+
+/*
+6
+Create a function called repeatStr2
+that takes 2 string parameters,
+and return the first string number time
+(of the lenght of the secend string) with space betweeen it
+Important: the stop condition should be [somthing=== 0]
+Ex: repeatStr2("ro","cc"); => "ro ro"
+Ex: repeatStr2("ro","fff"); => "ro ro ro"
+*/
+
+function repeatStr2( str , str2){
+  num=str2.length;
+  
+  if (num===0){
+
+    return "";
+  }
+
+  var sam=str2.slice(0,str2.length-1);
+  return(str+""+repeatStr2(str,sam))
+}
+console.log(repeatStr2("amam","aaaa"));
+
+
+/*/*
+7
+Create a function called multiOf
+that takes 3 parameters,
+and return the first number mutiple by
+(the second one ^ the third number)
+Ex: multiOf(4,10,3); => 4000
+Ex: multiOf(6,3,2); => 54
+Ex: multiOf(6,2,3); => 48
+*/
+
+function multiOf(num1,num2,num3 ){
+
+  if (num3===0){
+    return num1;
+
+  }
+ 
+ 
+  return num2*multiOf(num1, num2,num3-1);
+  
+} 
+
+
+multiOf(4,10,2)
 
 
 /*
-
-
-
-
+8
+Create a function called muti2
+that take two parameter
+and will return the multiplication from the first number to the second number
+Ex: muti2(4, 5); => 4 * 5 => 20
+Ex: muti2(3, 6); => 3 * 4 * 5 * 6 => 360
 */
+
+
+function muti2(num1,num2){
+
+  if(num1===num2){
+  
+    return num2;
+  }
+  
+  return (num1 * muti2(  (num1+1) , num2  )  );
+  }
+
+  muti2(4,5)
+
 
 
 /*
-
-
-
-
+9
+Create a function called numberBetweenUs
+that take 2 parameters
+and return the number between them
+** Important: You should dint have extra space at the end or comma
+** Important: the stop condition should [num1===num2-1]
+** hint: maybe you will have 2 if statment
+numberBetweenUs(2,8) => "3, 4, 5, 6, 7"
+numberBetweenUs(1,3) => "2"
 */
+
+function numberBetweenUs(num1 , num2){
+  if (num1===num2-2)  {
+     
+    return num2-1;
+
+    } 
+    
+  
+  return (num1+1)+" , "+(numberBetweenUs(  (num1+1) , num2  )  );
+}
+numberBetweenUs(2,8);
+
+/*
+10
+Write a function called countDown
+that takes one parameter
+and return a string represent the count down from the number
+that insert to 0
+instead of 0 need to ahve "done"
+between each number a comma and space
+** you need to use recursion
+** Important: You should dint have extra space at the end or comma
+countDown(5)
+=> "5, 4, 3, 2, 1, done"
+countDown(2)
+=> "2, 1, done"
+countDown(7)
+=> "7, 6, 5, 4, 3, 2, 1, done"
+*/
+function countDown(x){
+
+if(x===0){
+  return "done";
+}
+return x = x +","+countDown(x-1);
+}
+console.log(countDown(2))
+
+
 
 
 /*
-
-
-
-
+11
+Write a function called multiplication2
+that takes two parameters
+and return the multiplication of them
+you need to use only sum
+multiplication2(5,4) => 20
+multiplication2(2,8) => 16
+multiplication2(7,6) =>  42
 */
+ function multiplication2(num1,num2){
+     if(num2===0){
+       return 0;
+     }
+    return (num1 + multiplication2(num1,num2-1));
 
+ }
+
+   multiplication2(5,4)
+
+
+// 11
+// Write a function called mod2
+// that takes two parameters
+// and return the module of them
+// without using %
+// mod2(5,4) => 1
+// mod2(2,8) => 2
+// mod2(7,4) => 3
+// */
+function mod2(num1,num2){
+  if(num1<num2){
+   return num1;
+  }
+  
+  return  mod2(num1-num2,num2 )
+}
+mod2(8,5)
 
 /*
-
-
-
-
+12
+Write a function called mirror
+that takes one parameter a string
+and return the reversed string
+**hint: "str"[0] => "s"
+**hint: "car"[1] => "a"
+** Important: You should dint have extra space at the end
+mirror("school") => "loohcs"
+mirror("car") => "rac"
+mirror("maDrasa") => "asarDam"
 */
-
 
 /*
-
-
-
-
+13
+Write a function called mirrorCaseAlso
+that takes one parameter a string
+and return the reversed string order
+and the lower case to upper case
+and the upper case to lower case
+**hint: seacrh in MDN how "a" => "A" and "A" => "a"
+** Important: You should dint have extra space at the end
+mirrorCaseAlso("sChOol") => "LOoHcS"
+mirrorCaseAlso("THOR") => "roht"
+mirrorCaseAlso("BaBa") => "AbAb"
 */
-
 
 /*
-
-
-
-
+14
+Write a function called repeatChar
+that takes two parameters
+and return the times that this char
+without case sensetive
+repeat inside this string
+**hint: "str"[0] => "s"
+**hint: "car"[1] => "a"
+repeatChar("schOol","o") => 2
+repeatChar("school","a") => 0
+repeatChar("School","s") => 1
+try more case by yourself
 */
-
 
 /*
-
-
-
-
+15
+Write a function called repeatChar2
+that takes two parameters
+and return the times that this char
+wit case sensetive
+repeat inside this string
+**hint: "str"[0] => "s"
+**hint: "car"[1] => "a"
+**hint: seacrh in MDN how "a" => "A" and "A" => "a"
+repeatChar2("schOol","o") => 1
+repeatChar2("school","a") => 0
+repeatChar2("School","S") => 1
+try more case by yourself
 */
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
